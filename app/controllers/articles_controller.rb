20a61031976471d5c9c47cf77bad31d72d2baea2
MagdaBM <buczek.magdalena@gmail.com>
2018-12-02 09:30:33 +0100

    Article params move to method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
    end
 
    def create
-      article_params = params.require(:article).permit(:title, :text)
+      article_params
       @article = Article.new(article_params)
       if @article.save
          redirect_to article_path(@article)
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
 
    def update
       @article = Article.find(params[:id])
-      article_params = params.require(:article).permit(:title, :text)
+      article_params
       if @article.update(article_params)
          redirect_to article_path(@article)
       else
@@ -40,4 +40,11 @@ class ArticlesController < ApplicationController
       @article.destroy
       redirect_to articles_path
    end
+
+   private
+
+   def article_params
+      article_params = params.require(:article).permit(:title, :text)
+   end
+
 end
