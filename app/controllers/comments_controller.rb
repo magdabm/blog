78460d1e0fc26f843907b0ab90339b87694cfb5e
MagdaBM <buczek.magdalena@gmail.com>
2018-12-16 13:31:23 +0100

    Add user as commenter, delete commente field, add authorization for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,15 @@
 class CommentsController < ApplicationController
    http_basic_authenticate_with name: "admin", password: "secret", only: :destroy
    before_action :find_article
+   before_action :authenticate_user!
 
    def create
       @comment = Comment.new(comment_params)
       @comment.article = @article
+      @comment.user = current_user if current_user
       if @comment.save
          flash[:notice] = "Your comment has been added to the article."
          redirect_to article_path(@article)
-         session[:commenter] = @comment.commenter
       else
          render 'articles/show'
       end
@@ -16,6 +17,7 @@ class CommentsController < ApplicationController
 
    def destroy
       @comment = Comment.find(params[:id])
+      return unless authorize_comment
       @comment.destroy
       flash[:notice] = "Your comment has been deleted."
       redirect_to article_path(@article)
@@ -23,10 +25,13 @@ class CommentsController < ApplicationController
 
    def edit
       @comment = Comment.find(params[:id])
+      return unless authorize_comment
    end
 
    def update
       @comment = Comment.find(params[:id])
+      return unless authorize_comment
+
       if @comment.update(comment_params)
          flash[:notice] = "Your comment has been updated."
          redirect_to article_path(@article)
@@ -38,12 +43,22 @@ class CommentsController < ApplicationController
 
    private
 
+   def authorize_comment
+      if @comment.user != current_user && !current_user&.admin?
+         flash[:alert] = "You are not allowed to be here"
+         redirect_to welcome_index_path
+         false
+      else
+         true
+      end
+   end
+
    def find_article
       @article = Article.find(params[:article_id])
    end
 
    def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:body, :user)
    end
 
 end
---
+++ db/migrate/20181216102804_add_user_to_comments.rb
@@ -0,0 +1,6 @@
+class AddUserToComments < ActiveRecord::Migration[5.2]
+  def change
+      add_reference :comments, :user
+      add_foreign_key :comments, :users
+  end
+end
