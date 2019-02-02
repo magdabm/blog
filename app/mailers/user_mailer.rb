fa458ecca009f658bc4e8eb254d20d2239d1ace7
MagdaBM <buczek.magdalena@gmail.com>
2019-02-02 12:10:29 +0100

    Add email notification

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+
+  default from: 'form@example.com'
+
+  def liked_article(user)
+    @user = user
+    mail(to: @user.email, subject: 'Liked article')
+  end
+
+end
